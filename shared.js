1d3f2ff137737af653a45f8b85ece570f5084569
islomnumanov <islom4ik.numanov@gmail.com>
2021-08-26 11:14:14 +0900

    JS and CSS

--- shared.js
+++ shared.js
@@ -2,6 +2,8 @@ const backdrop = document.querySelector(".backdrop");
 const modal = document.querySelector(".modal");
 const selectPlanButtons = document.querySelectorAll(".plan button");
 const modalNoButton = document.querySelector(".modal__action--negative");
+const toggleButton = document.querySelector(".toggle-button");
+const mobileNav = document.querySelector(".mobile-nav");
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function () {
@@ -17,4 +19,10 @@ closeModal = () => {
 
 modalNoButton.addEventListener("click", closeModal);
 
-backdrop.addEventListener("click", closeModal);
+backdrop.addEventListener("click", () => {
+  (mobileNav.style.display = "none"), closeModal();
+});
+
+toggleButton.addEventListener("click", () => {
+  (mobileNav.style.display = "block"), (backdrop.style.display = "block");
+});
